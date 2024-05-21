a753c4213d7db00316db73d602602725c6f11604
kaio_rama <ramacanevari1@gmail.com>
2024-05-21 19:41:18 -0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-## Realiza tu push tranquilo!
---
+++ precache-manifest.3b0686c320b9055a59d2c2ed136846b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3042ba124217d110d3a497848aacff70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d18030413227d7626f",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "3b6a60a9657f31a9244f",
+    "url": "/static/css/main.370b2315.chunk.css"
+  },
+  {
+    "revision": "e1d18030413227d7626f",
+    "url": "/static/js/2.a5136d71.chunk.js"
+  },
+  {
+    "revision": "b2fbd2d837aab2eccf5b742d1e543489",
+    "url": "/static/js/2.a5136d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aade6220b9f08b500f16",
+    "url": "/static/js/3.b7a00004.chunk.js"
+  },
+  {
+    "revision": "3b6a60a9657f31a9244f",
+    "url": "/static/js/main.6d9c972a.chunk.js"
+  },
+  {
+    "revision": "a4544fab552986392cce",
+    "url": "/static/js/runtime-main.20cca245.js"
+  },
+  {
+    "revision": "29e1dc59c8e642ea9ff7c700780a7eb2",
+    "url": "/static/media/banner.29e1dc59.jpeg"
+  },
+  {
+    "revision": "c845b4ca82d0372da6bc52f46bc9f433",
+    "url": "/static/media/carrousel2.c845b4ca.png"
+  },
+  {
+    "revision": "6dade403f4165f5d6cda75341a16a829",
+    "url": "/static/media/inicio1.6dade403.png"
+  },
+  {
+    "revision": "7d3e442768cfd941d8dffc7dc01b8151",
+    "url": "/static/media/inicio2.7d3e4427.png"
+  },
+  {
+    "revision": "d1ad356d6b8ec51a3124607e5adf2424",
+    "url": "/static/media/inicio3.d1ad356d.png"
+  },
+  {
+    "revision": "03c6a4e2ff6a4a332fae925f136dc61c",
+    "url": "/static/media/inicio4.03c6a4e2.png"
+  }
+]);
